179b79e8e3ce88307921dcfc15bf8adb14c137a6
joshram <josem.ramirez@gmail.com>
2025-01-15 09:27:58 -0500

    Update experiment.py
    compile False

--- templates/nanoGPT_lite/experiment.py
+++ templates/nanoGPT_lite/experiment.py
@@ -355,7 +355,7 @@ def train(dataset="shakespeare_char", out_dir="run_0", seed_offset=0):
         if torch.cuda.is_available() and torch.cuda.is_bf16_supported()
         else "float16"
     )  # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
-    compile = True  # do not torch compile the model on macbooks
+    compile = False  # do not torch compile the model on macbooks
 
     # various inits, derived attributes, I/O setup
     # if not ddp, we are running on a single gpu, and one process
