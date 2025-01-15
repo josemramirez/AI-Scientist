0af9af1d2e89513005ded43edfa6fe9a5618398b
joshram <josem.ramirez@gmail.com>
2025-01-15 08:55:11 -0500

    Update experiment.py
    cuda de nuevo

--- templates/nanoGPT_lite/experiment.py
+++ templates/nanoGPT_lite/experiment.py
@@ -349,7 +349,7 @@ def train(dataset="shakespeare_char", out_dir="run_0", seed_offset=0):
     # DDP settings
     backend = "nccl"  # 'nccl', 'gloo', etc.
     # system
-    device = "cpu"  # Always use CUDA
+    device = "cuda"  # Always use CUDA
     dtype = (
         "bfloat16"
         if torch.cuda.is_available() and torch.cuda.is_bf16_supported()
